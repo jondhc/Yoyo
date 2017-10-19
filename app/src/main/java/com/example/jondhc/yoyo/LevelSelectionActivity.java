45bc784ce09530c75f4ce810d082508875ae796f
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2017-10-19 01:02:48 -0500

    Changed LevelSelectio Activity layout to RelativeLayout only.

--- app/src/main/java/com/example/jondhc/yoyo/LevelSelectionActivity.java
+++ app/src/main/java/com/example/jondhc/yoyo/LevelSelectionActivity.java
@@ -1,21 +1,21 @@
 package com.example.jondhc.yoyo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class LevelSelectionActivity extends AppCompatActivity {
     Button btnVolver, btnSalir;
-    //Se puede, vaya que si se puede!
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_selection);
 
-        btnVolver = (Button)findViewById(R.id.btnVolver);
-        btnSalir = (Button)findViewById(R.id.btnSalir);
+        btnVolver = (Button) findViewById(R.id.btnVolver);
+        btnSalir = (Button) findViewById(R.id.btnSalir);
 
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/jondhc/yoyo/MainActivity.java
+++ app/src/main/java/com/example/jondhc/yoyo/MainActivity.java
@@ -2,13 +2,10 @@ package com.example.jondhc.yoyo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,11 +18,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        playBtn = (Button)findViewById(R.id.playButton);
+        playBtn = (Button) findViewById(R.id.playButton);
 
-        playBtn.setOnClickListener(new View.OnClickListener(){
+        playBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LevelSelectionActivity.class);
                 startActivity(intent);
             }
