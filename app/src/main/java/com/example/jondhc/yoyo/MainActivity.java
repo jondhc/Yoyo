b0ebf64c79bad0b24ad9bd7b4ef19ff409ecc01d
ArmandoV <harmandov>
2017-10-18 21:55:12 -0500

    Se agregaron botones de Volver y Salir en actividad de selection de menú

--- app/src/main/java/com/example/jondhc/yoyo/MainActivity.java
+++ app/src/main/java/com/example/jondhc/yoyo/MainActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
