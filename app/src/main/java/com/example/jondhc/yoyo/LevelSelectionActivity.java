811bef134b39ef49d8c8e97b8dd9f3369e16a4a3
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2017-10-31 14:58:48 -0600

    Main screen fixed

--- app/src/main/java/com/example/jondhc/yoyo/LevelSelectionActivity.java
+++ app/src/main/java/com/example/jondhc/yoyo/LevelSelectionActivity.java
@@ -3,7 +3,6 @@ package com.example.jondhc.yoyo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 
 public class LevelSelectionActivity extends AppCompatActivity {
@@ -14,9 +13,9 @@ public class LevelSelectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_selection);
 
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN; //Hide the status bar
-        decorView.setSystemUiVisibility(uiOptions);
+        //View decorView = getWindow().getDecorView();
+        //int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN; //Hide the status bar
+        //decorView.setSystemUiVisibility(uiOptions);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
