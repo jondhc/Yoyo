08772313551458f05528ed1dff256dc8e181c388
Unknown <clement.ogame@gmail.com>
2017-10-31 15:26:09 -0600

    Add Glide, LevelSelectionScreen display image and scroll

---
+++ app/src/main/java/com/example/jondhc/yoyo/GlideApi.java
@@ -0,0 +1,8 @@
+package com.example.jondhc.yoyo;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class GlideApi extends AppGlideModule {
+}
--- app/src/main/java/com/example/jondhc/yoyo/LevelSelectionActivity.java
+++ app/src/main/java/com/example/jondhc/yoyo/LevelSelectionActivity.java
@@ -1,9 +1,20 @@
 package com.example.jondhc.yoyo;
 
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class LevelSelectionActivity extends AppCompatActivity {
     Button backBtn, exitBtn;
@@ -12,8 +23,17 @@ public class LevelSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_selection);
-
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        double temp = width * 4.5312;
+        int height = (int) temp;
         View decorView = getWindow().getDecorView();
+
+        ImageView iview = (ImageView) findViewById(R.id.lvbackgroundimg);
+        GlideApp.with(this).load(R.drawable.lselection_background).override(width,height).into(iview);
+
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN; //Hide the status bar
         decorView.setSystemUiVisibility(uiOptions);
 
