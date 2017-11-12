cb0900e237c5453716eea95af8a49450a8cf9f2c
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2017-11-11 23:15:19 -0600

    StatusBar hidden from all activities.

--- app/src/main/java/com/example/jondhc/yoyo/Level1Activity.java
+++ app/src/main/java/com/example/jondhc/yoyo/Level1Activity.java
@@ -3,23 +3,27 @@ package com.example.jondhc.yoyo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Level1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_level1);
 
-        // Hide ActionBar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN; //Hide the status bar
+        decorView.setSystemUiVisibility(uiOptions);
+
         ActionBar actionBar = getSupportActionBar();
-        if(actionBar != null)
-            actionBar.hide();
+        if (actionBar != null)
+            actionBar.hide();   // Hide ActionBar
 
+        setContentView(R.layout.activity_level1);
     }
 
     @Override
-    public boolean onSupportNavigateUp(){
+    public boolean onSupportNavigateUp() {
         finish();
         return true;
     }
