848150fc76b875e37c239bb20b9024d45a8a8a56
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2017-11-18 23:53:27 -0600

    Added comments for documentation.

--- app/src/main/java/com/example/jondhc/yoyo/Level1Activity.java
+++ app/src/main/java/com/example/jondhc/yoyo/Level1Activity.java
@@ -18,35 +18,39 @@ import java.util.TimerTask;
 public class Level1Activity extends AppCompatActivity {
 
     int basket_counter = 0; //Counter for the fruits
-    public int quantity;
-    public View char_cat;
+    public int quantity;    //Quantity of fruits to display
+    public View char_cat;   //Variable for cat imageView
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Random randomGenerator = new Random();
-        int maxFruits = 9;
-        int minFruits = 1;
-        int range = maxFruits - minFruits + 1;
-        quantity = randomGenerator.nextInt(range) + minFruits;
-        System.out.println("Cantidad de frutas: " + quantity);
+        Random randomGenerator = new Random();  //Random generator for fruits number
+        int maxFruits = 9;  //Maximum Number of fruits to display
+        int minFruits = 1;  //Minimum number of fruits to display
+        int range = maxFruits - minFruits + 1;  //Range of fruits to be generated randomly
+        quantity = randomGenerator.nextInt(range) + minFruits;  //Setting number between the range
+        System.out.println("Cantidad de frutas: " + quantity);  //Printing to console the number of fruits to display
 
+        //Hiiding the status bar
         View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN; //Hide the status bar
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+        //end of Hiding the status bar
 
+        // Hiding action bar
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null)
-            actionBar.hide();   // Hide ActionBar
+            actionBar.hide();
+        //End of hiding action bar
 
-        setContentView(R.layout.activity_level1);
+        setContentView(R.layout.activity_level1);   //Set the contentView on the screen
 
-        TextView counter = (TextView) findViewById(R.id.count_of_fruits);
-        counter.setText(String.valueOf(basket_counter));
+        TextView counter = (TextView) findViewById(R.id.count_of_fruits);   //textView to display the counter of collected fruits
+        counter.setText(String.valueOf(basket_counter));    //set the text on the counter to the number of collected fruits
 
-        //Adding drag and drop functionality
+        //Adding drag and drop functionality to every berry.
         View berry1 = findViewById(R.id.berry1);
         berry1.setOnTouchListener(new DragTouchListener());
         View berry2 = findViewById(R.id.berry2);
@@ -65,7 +69,9 @@ public class Level1Activity extends AppCompatActivity {
         berry8.setOnTouchListener(new DragTouchListener());
         View berry9 = findViewById(R.id.berry9);
         berry9.setOnTouchListener(new DragTouchListener());
+        //end of Adding drag and drop functionality to every berry.
 
+        //Switch-case for the quantity of fruits to be generated
         switch (quantity) {
             case 1:
                 berry4.setVisibility(View.VISIBLE);
@@ -130,47 +136,48 @@ public class Level1Activity extends AppCompatActivity {
                 berry8.setVisibility(View.VISIBLE);
                 berry9.setVisibility(View.VISIBLE);
                 break;
-        }
+        }//end Switch
+        //end of Switch-case for the quantity of fruits to be generated
 
 
-        View cat = findViewById(R.id.cat_catch);
-        cat.setOnClickListener(new View.OnClickListener() {
+        View cat = findViewById(R.id.cat_catch);    //Getting the character ImageView
+        cat.setOnClickListener(new View.OnClickListener() { //Setting action when character is touched
             @Override
             public void onClick(View v) {
-                MediaPlayer initialInstructions = MediaPlayer.create(getApplicationContext(), R.raw.instructions_g);
-                initialInstructions.start();
+                MediaPlayer initialInstructions = MediaPlayer.create(getApplicationContext(), R.raw.instructions_g);    //Set audio to instructions audio
+                initialInstructions.start();    //Play instructions audio
             }
         });//end setOnClickListener
     }//end onCreate
 
     @Override
-    public boolean onSupportNavigateUp() {
+    public boolean onSupportNavigateUp() {  //Method to show action bar, not used
         finish();
         return true;
     }//end onSupportNavigateUp
 
-    class userWon extends TimerTask {
+    class userWon extends TimerTask {   //Runnable for congratulations action to be delayed
         public void run() {
-            MediaPlayer winning = MediaPlayer.create(getApplicationContext(), R.raw.won);
-            winning.start();
+            MediaPlayer winning = MediaPlayer.create(getApplicationContext(), R.raw.won);   //set audio to user-won audio
+            winning.start();    //Play audio
         }
     }
 
 
-    private final class DragTouchListener implements View.OnTouchListener {
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                ClipData data = ClipData.newPlainText("", "");
-                View.DragShadowBuilder shadowbuiler = new View.DragShadowBuilder(view);
-                view.startDrag(data, shadowbuiler, view, 0);
-                view.setVisibility(View.INVISIBLE);
-                basket_counter += 1;
-                TextView counter = (TextView) findViewById(R.id.count_of_fruits);
-                counter.setText(String.valueOf(basket_counter));
+    private final class DragTouchListener implements View.OnTouchListener { //Method to execute when user drags or touches a fruit
+        public boolean onTouch(View view, MotionEvent motionEvent) {    //Using as paramters the view which is interacted with and the motion
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {   //When user selects a fruit
+                ClipData data = ClipData.newPlainText("", "");  //Text to keep in clipboard if needed.
+                View.DragShadowBuilder shadowbuilder = new View.DragShadowBuilder(view);
+                view.startDrag(data, shadowbuilder, view, 0);
+                view.setVisibility(View.INVISIBLE); //When finished dragging, set View as invisible
+                basket_counter += 1;    //Add one when a fruit is selected
+                TextView counter = (TextView) findViewById(R.id.count_of_fruits);   //Get the counter of collected fruits
+                counter.setText(String.valueOf(basket_counter));    //set the basket counter to the number of collected fruits
 
                 //Media player
-                int audio = R.raw.one;
-                switch (basket_counter) {
+                int audio = 0;
+                switch (basket_counter) {   //For every counted fruit, select the according audio
                     case 1:
                         audio = R.raw.one;
                         break;
@@ -198,21 +205,22 @@ public class Level1Activity extends AppCompatActivity {
                     case 9:
                         audio = R.raw.nine;
                         break;
-                }
-                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), audio);
-                mp.start();
-                if (basket_counter == quantity) {
-                    Timer timer = new Timer();
-                    timer.schedule(new userWon(), 1000);
-                    //System.out.println("User won!");
-                    char_cat = findViewById(R.id.cat_catch);
+                }//end switch
+                MediaPlayer mp = MediaPlayer.create(getApplicationContext(), audio);    //set the audio to selected audio
+                mp.start(); //Play the audio of the number of fruits.
+                if (basket_counter == quantity) {   //When all the fruits are collected
+                    Timer timer = new Timer();  //Timer for Winning audio to run
+                    timer.schedule(new userWon(), 1000);    //Setting delay for audio
+                    System.out.println("User won!");
+                    //Animation of character
+                    char_cat = findViewById(R.id.cat_catch);    //Getting character image
                     RotateAnimation char_anim = new RotateAnimation(0f, 360f, char_cat.getWidth() / 2, char_cat.getHeight() / 2);
                     char_anim.setInterpolator(new LinearInterpolator());
                     char_anim.setRepeatCount(0);
                     char_anim.setDuration(700);
                     char_cat.startAnimation(char_anim);
-
-                }
+                    //ends Animation of character
+                }//end if
                 return true;
             }//end if
             else {
@@ -220,6 +228,4 @@ public class Level1Activity extends AppCompatActivity {
             }//end else
         }//end onTouch
     } //end DragTouchListener
-
-
-}
+}//end Level1Activity
