de9915e87e5a2d6375766b1a04d5381ff9d07baf
Jesus10av <jesus.andradevz@udlap.mx>
2017-12-01 20:19:08 -0600

    se generan los globos aleatoriamente

--- app/src/main/java/com/example/jondhc/yoyo/LevelSum.java
+++ app/src/main/java/com/example/jondhc/yoyo/LevelSum.java
@@ -8,7 +8,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class LevelSum extends AppCompatActivity {
+
+    public int muestraglobos;
 
 
 
@@ -37,6 +41,59 @@ public class LevelSum extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null)
             actionBar.hide();
+
+        Random randomGenerator = new Random();  //Random generator for fruits number
+        int maxGlobos = 6;  //Maximum Number of fruits to display
+        int minGlobos = 1;  //Minimum number of fruits to display
+        int range = maxGlobos - minGlobos + 1;  //Range of fruits to be generated randomly
+        muestraglobos = randomGenerator.nextInt(range) + minGlobos;  //Setting number between the range
+        System.out.println("Cantidad de Globos: " + muestraglobos);  //Printing to console the number of fruits to display
+
+        View globo1 = findViewById(R.id.airballon_sky);
+        View globo2 = findViewById(R.id.airballon2_sky);
+        View globo3 = findViewById(R.id.airballon3_sky);
+        View globo4 = findViewById(R.id.airballon4_sky);
+        View globo5 = findViewById(R.id.airballon5_sky);
+        View globo6 = findViewById(R.id.airballon6_sky);
+
+
+        switch (muestraglobos) {
+            case 1:
+                globo2.setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                globo1.setVisibility(View.VISIBLE);
+                globo3.setVisibility(View.VISIBLE);
+                break;
+            case 3:
+                globo1.setVisibility(View.VISIBLE);
+                globo2.setVisibility(View.VISIBLE);
+                globo3.setVisibility(View.VISIBLE);
+                break;
+            case 4:
+                globo1.setVisibility(View.VISIBLE);
+                globo3.setVisibility(View.VISIBLE);
+                globo4.setVisibility(View.VISIBLE);
+                globo6.setVisibility(View.VISIBLE);
+                break;
+            case 5:
+                globo1.setVisibility(View.VISIBLE);
+                globo2.setVisibility(View.VISIBLE);
+                globo3.setVisibility(View.VISIBLE);
+                globo4.setVisibility(View.VISIBLE);
+                globo5.setVisibility(View.VISIBLE);
+                break;
+            case 6:
+                globo1.setVisibility(View.VISIBLE);
+                globo2.setVisibility(View.VISIBLE);
+                globo3.setVisibility(View.VISIBLE);
+                globo4.setVisibility(View.VISIBLE);
+                globo5.setVisibility(View.VISIBLE);
+                globo6.setVisibility(View.VISIBLE);
+                break;
+        }
+
+
 
 
     }
