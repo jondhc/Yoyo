e7139c2c279b71ac36a27750bb79171f66e7a540
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2017-12-01 22:44:02 -0600

    Added Google Authentication. (Integrating Google Sign-In into Your Android App)

--- app/src/main/java/com/example/jondhc/yoyo/MainActivity.java
+++ app/src/main/java/com/example/jondhc/yoyo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jondhc.yoyo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +17,8 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.auth.api.Auth;
+
 
 import static com.example.jondhc.yoyo.Characters.CAT;
 
@@ -25,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     Button playBtnDog; //Initializing Dog Play Button
     GlobalApplication mApp;
     private GoogleSignInClient mGoogleSignInClient;
+    private static int RC_SIGN_IN = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +50,13 @@ public class MainActivity extends AppCompatActivity {
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-
+        View yoyoSignIn = findViewById(R.id.main_screen_text);
+        yoyoSignIn.setOnClickListener(new View.OnClickListener() { //Detect touch on button
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
 
         playBtnCat = (Button) findViewById(R.id.playButtonCat);   //Detect the play button
         playBtnCat.setOnClickListener(new View.OnClickListener() { //Detect touch on button
@@ -65,5 +76,46 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(playI);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //updateUI(account);
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            //Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            //updateUI(null);
+        }
     }
 }
