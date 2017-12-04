9c71395fa0a57636183f247a0faf39a8b5d3374d
Jesus10av <jesus.andradevz@udlap.mx>
2017-12-04 03:10:59 -0600

    nivel de geometria terminado nivel de números terminado

--- app/src/main/java/com/example/jondhc/yoyo/Level_phone.java
+++ app/src/main/java/com/example/jondhc/yoyo/Level_phone.java
@@ -1,13 +1,17 @@
 package com.example.jondhc.yoyo;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Level_phone extends AppCompatActivity {
 
-    //private int contador = 0;
+    private int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +43,13 @@ public class Level_phone extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setVisibility(View.INVISIBLE);
+                MediaPlayer mp0 = MediaPlayer.create(getApplicationContext(), R.raw.circulo);   //set audio to user-won audio
+                mp0.start();
+                contador ++;
+                if (contador == 10){
+                    Timer timer = new Timer();  //Timer for Winning audio to run
+                    timer.schedule(new ganaste2(), 1000);    //Setting delay for audio
+                }
 
             }
         });
@@ -48,6 +59,13 @@ public class Level_phone extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setVisibility(View.INVISIBLE);
+                MediaPlayer mp1 = MediaPlayer.create(getApplicationContext(), R.raw.one);   //set audio to user-won audio
+                mp1.start();
+                contador ++;
+                if (contador == 10){
+                    Timer timer = new Timer();  //Timer for Winning audio to run
+                    timer.schedule(new ganaste2(), 1000);    //Setting delay for audio
+                }
 
             }
         });
@@ -57,6 +75,13 @@ public class Level_phone extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setVisibility(View.INVISIBLE);
+                MediaPlayer mp2 = MediaPlayer.create(getApplicationContext(), R.raw.two);   //set audio to user-won audio
+                mp2.start();
+                contador ++;
+                if (contador == 10){
+                    Timer timer = new Timer();  //Timer for Winning audio to run
+                    timer.schedule(new ganaste2(), 1000);    //Setting delay for audio
+                }
 
             }
         });
@@ -66,6 +91,13 @@ public class Level_phone extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setVisibility(View.INVISIBLE);
+                MediaPlayer mp3 = MediaPlayer.create(getApplicationContext(), R.raw.three);   //set audio to user-won audio
+                mp3.start();
+                contador ++;
+                if (contador == 10){
+                    Timer timer = new Timer();  //Timer for Winning audio to run
+                    timer.schedule(new ganaste2(), 1000);    //Setting delay for audio
+                }
 
             }
         });
@@ -75,6 +107,13 @@ public class Level_phone extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setVisibility(View.INVISIBLE);
+                MediaPlayer mp4 = MediaPlayer.create(getApplicationContext(), R.raw.four);   //set audio to user-won audio
+                mp4.start();
+                contador ++;
+                if (contador == 10){
+                    Timer timer = new Timer();  //Timer for Winning audio to run
+                    timer.schedule(new ganaste2(), 1000);    //Setting delay for audio
+                }
 
             }
         });
@@ -84,6 +123,13 @@ public class Level_phone extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setVisibility(View.INVISIBLE);
+                MediaPlayer mp5 = MediaPlayer.create(getApplicationContext(), R.raw.five);   //set audio to user-won audio
+                mp5.start();
+                contador ++;
+                if (contador == 10){
+                    Timer timer = new Timer();  //Timer for Winning audio to run
+                    timer.schedule(new ganaste2(), 1000);    //Setting delay for audio
+                }
 
             }
         });
@@ -93,6 +139,13 @@ public class Level_phone extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setVisibility(View.INVISIBLE);
+                MediaPlayer mp6 = MediaPlayer.create(getApplicationContext(), R.raw.six);   //set audio to user-won audio
+                mp6.start();
+                contador ++;
+                if (contador == 10){
+                    Timer timer = new Timer();  //Timer for Winning audio to run
+                    timer.schedule(new ganaste2(), 1000);    //Setting delay for audio
+                }
 
             }
         });
@@ -102,6 +155,13 @@ public class Level_phone extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setVisibility(View.INVISIBLE);
+                MediaPlayer mp7 = MediaPlayer.create(getApplicationContext(), R.raw.seven);   //set audio to user-won audio
+                mp7.start();
+                contador ++;
+                if (contador == 10){
+                    Timer timer = new Timer();  //Timer for Winning audio to run
+                    timer.schedule(new ganaste2(), 1000);    //Setting delay for audio
+                }
 
             }
         });
@@ -111,6 +171,13 @@ public class Level_phone extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setVisibility(View.INVISIBLE);
+                MediaPlayer mp8 = MediaPlayer.create(getApplicationContext(), R.raw.eight);   //set audio to user-won audio
+                mp8.start();
+                contador ++;
+                if (contador == 10){
+                    Timer timer = new Timer();  //Timer for Winning audio to run
+                    timer.schedule(new ganaste2(), 1000);    //Setting delay for audio
+                }
 
             }
         });
@@ -119,10 +186,24 @@ public class Level_phone extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.setVisibility(View.INVISIBLE);
+                MediaPlayer mp9 = MediaPlayer.create(getApplicationContext(), R.raw.nine);   //set audio to user-won audio
+                mp9.start();
+                contador ++;
+                if (contador == 10){
+                    Timer timer = new Timer();  //Timer for Winning audio to run
+                    timer.schedule(new ganaste2(), 1000);    //Setting delay for audio
+                }
 
             }
         });
 
+    }
+
+    class ganaste2 extends TimerTask {   //Runnable for congratulations action to be delayed
+        public void run() {
+            MediaPlayer winning = MediaPlayer.create(getApplicationContext(), R.raw.won);   //set audio to user-won audio
+            winning.start();    //Play audio
+        }
     }
 }
 
